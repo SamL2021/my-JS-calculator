0f3af8f48dd24abc631cadcb78a0507e6de0b2ff
Samantha Lippiatt <sgd@live.com.au>
2021-12-02 16:07:56 +1100

    First commit

---
+++ calculator.md
@@ -0,0 +1,47 @@
+My Calculator plan
+
+Part One - Design
+
+1. Find image online focusing on simple design to allow maximum time to focus on building functionality https://www.shutterstock.com/image-vector/white-calculator-icon-on-blue-background-220395709
+
+- has number keys from 0 to 9
+- has operator keys (+, -, /, \*, =)
+- has a display (which will render the current calculation) in a box at the top
+- has a "." key
+
+2. Review design and consider how best to build, using Flexbox and or grid
+3. Create HTML first including all buttons (adding classes or ID's in HTML relating to their operation to use for DOM manipulation).
+4. Create styles in SCSS as much as possible refactoring code as I go
+
+Part Two - JavaScript
+
+1. Once design is in place begin work on JavaScript.
+
+- Start by reviewing resources online to assist and considering all functions that must be complete, the basic functionalities of a calculator are:
+
+  - addition, subtraction, multiplication, division, delete, all-clear, and the ability to use decimal numbers in performing these operations.
+
+- Start by creating variables in JS and use DOM to select relevant elements for all buttons
+- Can group and select all numbers and operators using querySelectorAll because the buttons appear several times on the calculator
+
+- Now we can start to code to make the calculator work:
+  - Determine where we will display the output and create a function to do so
+  - Functions to clear() or delete() data
+  - Function that adds numbers clicked to the display appendNumber()
+  - Function to selectOperation(operator) from operators
+  - Functions to determineValues() and displayOutput()
+
+Outcomes:
+Should render the current calculation in a box at the top (calculator display) - will need to select
+It should handle decimals
+It doesn't need to support orders of operation
+It should not use eval() or Function() constructor
+You should be writing arrow functions
+Try and create as many pure functions as possible!
+
+If time allows or in the future I would like to create inverted colors as well https://www.shutterstock.com/image-vector/blue-calculator-icon-on-white-background-233412259
+
+// Additional buttons
+// const memoryRecallBtn = document.getElementById("memory-recall");
+// const memoryPlusBtn = document.getElementById("memory-plus");
+// const memorySubtractBtn = document.getElementById("memory-subtract");
---
+++ script.js
@@ -0,0 +1,124 @@
+// select all buttons
+const numberBtns = document.querySelectorAll("[data-number]");
+const operationBtns = document.querySelectorAll("[data-operation]");
+const equalsBtn = document.querySelector("[data-equals]");
+const deleteBtn = document.querySelector("[data-delete]");
+const clearAllButton = document.querySelector("[data-all-clear]");
+const previousText = document.querySelector("[data-previous-operand]");
+const currentText = document.querySelector("[data-current-operand]");
+let currentOperand = "0";
+let previousOperand = "0";
+let operator = "";
+
+// update the values inside the output also need to make sure only one decimal
+const updateDisplay = () => {};
+
+// Number Btns
+numberBtns.forEach((button) => {
+    button.addEventListener("click", () => {
+        // console.log(button.innerHTML);
+        submitNumber(button.innerHTML.trim());
+    });
+});
+
+// Get values from numbers
+const submitNumber = (x) => {
+    if (currentOperand === "0") {
+        currentOperand = x;
+    } else {
+        currentOperand = currentOperand + x;
+    }
+    console.log(currentOperand);
+};
+
+// Operation Btns
+operationBtns.forEach((button) => {
+    button.addEventListener("click", () => {
+        submitOperator(button.innerHTML.trim());
+        selectOperation(button.innerHTML);
+        calculate();
+        // updateDisplay();
+    });
+});
+
+// Get values from operations
+const submitOperator = (x) => {
+    operator = x;
+    console.log(operator);
+};
+
+equalsBtn.addEventListener("click", (button) => {
+    submitEquals();
+});
+
+const submitEquals = (x) => {
+    console.log("Equals");
+    console.log(calculate());
+};
+
+// All Clear Button "AC"
+clearAllButton.addEventListener("click", (button) => {
+    submitAllClear();
+    // console.log("Clear");
+});
+
+// All Clear function "AC"
+const submitAllClear = (x) => {
+    currentOperand = "0";
+    previousOperand = "";
+    // operation = undefined;
+    console.log(currentOperand);
+};
+
+// Delete Button "C"
+deleteBtn.addEventListener("click", (button) => {
+    submitDelete();
+    // console.log("delete");
+});
+
+// Delete Button "C"
+const submitDelete = (x) => {
+    if (currentOperand.length === 1) {
+        currentOperand = "0";
+    } else {
+        currentOperand = currentOperand.slice(0, -1);
+    }
+
+    console.log(currentOperand);
+};
+
+// Calculates and generates result.
+const calculate = () => {
+    let calculation;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (calculation) {
+        case "+":
+            calculation = num1 + num2;
+            break;
+        case "-":
+            calculation = num1 - num2;
+            break;
+        case "x":
+            calculation = num1 * num2;
+            break;
+        case "÷":
+            calculation = num1 / num2;
+            break;
+        default:
+            return;
+    }
+};
+
+const selectOperation = (operation) => {
+    if (currentOperand === "") return;
+    if (previousOperand !== "")
+        // If history has value then compute (How do we get values into history?)
+        calculate(); //otherwise
+    operation = operation;
+    // done typing the previous number move to history
+    previousOperand = currentOperand;
+    currentOperand = "";
+};
